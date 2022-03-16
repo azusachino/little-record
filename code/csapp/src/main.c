329471c41b774804d15252839f6c80cb2803c856
azusachino <azusa146@gmail.com>
2022-03-16 11:46:28 +0800

    Fix the problem of tlpi code cannot compile

--- code/csapp/src/main.c
+++ code/csapp/src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+typedef unsigned char *pointer;
+
+void show_bytes(pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%p\t0x%.2x\n", start + i, start[i]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
+    int a = 15213;
+    show_bytes((pointer)&a, sizeof(int));
     printf("Hello CSAPP\n");
 }
